cf1c36bad6ccad604660a38081db07ae00d26818
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2017-09-14 19:05:54 +0100

    Tidied inky-compose.js, fixed issues when creating new emails, updated version numbers and descriptions

--- ang/inky-compose.js
+++ ang/inky-compose.js
@@ -1,13 +1,36 @@
 (function(angular, $, _) {
   // declare your module
-  module = angular.module('civicrm-inky-compose', ['ui.ace']);
+  inkyCompose = angular.module('civicrm-inky-compose', ['ui.ace']);
 
-  module.controller('InkyComposeController', function($scope, $sce, crmApi){
+  inkyCompose.controller('InkyComposeController', function($scope, $sce, crmApi){
 
+    // Initialise the form
+    if(!$scope.$parent.mailing.template_options.pug){
+      $scope.$parent.mailing.template_options.pug = "container\n row\n  columns\n   h1 Hello World\n   p I am an email."
+    }
+    $scope.$parent.mailing.body_html = trustAndAddReplaceMethod($scope.$parent.mailing.body_html)
+
+    // Call Civinky to convert the Pug and CSS into email friendly HTML
+    $scope.convert = function(){
+      try{
+        CRM.civinky({pug: $scope.$parent.mailing.template_options.pug, css: $scope.css}).then(function(result){
+          $scope.$parent.mailing.body_html = trustAndAddReplaceMethod(result)
+        });
+      }catch (e) {
+        CRM.alert(e)
+      }
+    }
+    $scope.convert()
+
+    // Watch for changes to css, and convert the Pug again if it changes
+    $scope.$watch('css', function(){
+      $scope.convert()
+    })
+
+    // When a valid CSS URL has been passed to template_options.cssUrl, get the
+    // CSS
     $scope.getCss = function(){
-      // console.log('being called')
-      if($scope.$parent.mailing.template_options.cssUrl){
-        // console.log('being called is')
+      if($scope.$parent.mailing.template_options.cssUrl && /\.css$/.test($scope.$parent.mailing.template_options.cssUrl)){
         $.get( $scope.$parent.mailing.template_options.cssUrl)
         .done(function(css){
           $scope.css = css
@@ -19,40 +42,22 @@
         $scope.css = ''
       }
     }
+    $scope.getCss()
 
-    $scope.$watch('css', function(){
-      $scope.convert()
-    })
-
-    function trustAndAddReplaceMethod(value){
+    // This function is needed because
+    // 1) by default Angular will santize the html that Civinky returns, and
+    // 2) crmApi wants to call the replace() method of mailing.body_html but the
+    //    trusted HTML object does not have this method (so we clone it from
+    //    a newString()). See CRM-18474 for more info.
+    function trustAndAddReplaceMethod(value = ''){
       value = $sce.trustAsHtml(value)
-
-      // Workaround: We need to add a replace method to this trusted object as it will be called in crmApi (see CRM-18474) when attempting a save.
       s = new String();
       value.replace = s.replace
-
       return value
     }
-    //
-    // TODO I suspect that there is a better way to initialise the template
-    $scope.$parent.mailing.body_html = trustAndAddReplaceMethod($scope.$parent.mailing.body_html)
-
-
-    $scope.convert = function(){
-
-      try{
-        CRM.civinky({pug: $scope.$parent.mailing.template_options.pug, css: $scope.css}).then(function(result){
-          $scope.$parent.mailing.body_html = trustAndAddReplaceMethod(result)
-        });
-      }catch (e) {
-        CRM.alert(e)
-      }
-    }
-    $scope.getCss()
-    $scope.convert()
   })
 
-  module.directive('crmMailingInkyCompose', function() {
+  inkyCompose.directive('crmMailingInkyCompose', function() {
     return {
       templateUrl: CRM.resourceUrls['civicrm-inky-compose'] + '/ang/inkyCompose.html',
       scope: {
@@ -60,9 +65,6 @@
       }
     };
   });
-
-
-
 
 
 })(angular, CRM.$, CRM._);
